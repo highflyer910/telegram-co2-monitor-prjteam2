e039c4afc437cabfc4fdc96b21709383dd9d75ee
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-06-16 01:39:31 +0400

    update

--- frontend/src/components/LandingPage/TelegramLoginButton.js
+++ frontend/src/components/LandingPage/TelegramLoginButton.js
@@ -15,9 +15,9 @@ const TelegramLoginButton = () => {
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?7';
     script.async = true;
-    script.setAttribute('data-telegram-login', 'AnotherCoBot');
+    script.setAttribute('data-telegram-login', 'co2usage_bot');
     script.setAttribute('data-size', 'large');
-    script.setAttribute('data-auth-url', 'https://c972-2a0b-6204-1911-3900-d527-70b5-ddb-a5af.ngrok-free.app/auth/telegram/callback'); // Replace with your auth URL
+    script.setAttribute('data-auth-url', 'https://gogreenapp.vercel.app/auth/telegram/callback'); // Replace with your auth URL
     script.setAttribute('data-request-access', 'write');
     script.setAttribute('data-onauth', 'onTelegramAuth(user)');
     document.getElementById('telegram-login-button-container').appendChild(script);
